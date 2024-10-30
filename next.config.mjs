667cdd7106198292dfe550833735d50184bcf310
tejas <tejas.tech573@gmail.com>
2024-10-30 17:26:30 +0530

    vercel deploy

--- next.config.mjs
+++ next.config.mjs
@@ -4,20 +4,6 @@ const nextConfig = {
     domains: ["i.pravatar.cc"],
   },
   productionBrowserSourceMaps: false,
-  async headers() {
-    return [
-      {
-        // Apply this to all routes
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: "upgrade-insecure-requests",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
